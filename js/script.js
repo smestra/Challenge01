eb039a7d596b937ca4fbb5571c3276594bf2f261
smestra <sebastianmestra1299@gmail.com>
2023-06-07 00:48:42 -0500

    Adding enter button and correcting final bugs

--- js/script.js
+++ js/script.js
@@ -18,12 +18,13 @@ let numbers = "";
 
 // funciones
 
-const fnA1 = () => {if (letters == "") {
+const fnA1 = () => {
+  if (letters == "") {
     letters = "A";
-  document.getElementById("letrasId").innerHTML = letters;
-} else { alert("Accion Invalida");
-}
-
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
+    alert("Accion Invalida");
+  }
 };
 
 const fnB2 = () => {
@@ -33,120 +34,108 @@ const fnB2 = () => {
   } else {
     alert("Accion Invalida");
   }
-  
 };
 
 const fnC3 = () => {
-  if (letters == "AB" ) {
+  if (letters == "AB") {
     letters += "C";
     document.getElementById("letrasId").innerHTML = letters;
   } else {
     alert("Accion Invalida");
   }
-  
 };
 
 const fnD4 = () => {
- if (letters == "ABC") {
-    letters += "D"
-    document.getElementById("letrasId").innerHTML= letters;
- } else {
-   alert("Accion Invalida") 
- }
+  if (letters == "ABC") {
+    letters += "D";
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
+    alert("Accion Invalida");
+  }
 };
 
-const fnE5 = () => { 
-    if (letters == "ABCD") {
-     letters += "E"   
-     document.getElementById("letrasId").innerHTML= letters;
-    } else {
-      alert("Accion Invalida")  
-    }
+const fnE5 = () => {
+  if (letters == "ABCD") {
+    letters += "E";
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
+    alert("Accion Invalida");
+  }
 };
 
 const fnF6 = () => {
- if ( letters == "ABCDE") {
-    letters += "F"
-    document.getElementById("letrasId").innerHTML= letters;
- } else {
-    alert("Accion Invalida")
- }
+  if (letters == "ABCDE") {
+    letters += "F";
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
+    alert("Accion Invalida");
+  }
 };
 
 const fnG7 = () => {
-if (letters == "ABCDEF") {
-    letters += "G"
-    document.getElementById("letrasId").innerHTML= letters;
-} else { alert("Accion Invalida")
-}
+  if (letters == "ABCDEF") {
+    letters += "G";
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
+    alert("Accion Invalida");
+  }
 };
 
 const fnH8 = () => {
-if (letters == "ABCDEFG") {
-    letters += "H"
-    document.getElementById("letrasId").innerHTML= letters;
-} else {
-alert("Accion Invalida")
-}
+  if (letters == "ABCDEFG") {
+    letters += "H";
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
+    alert("Accion Invalida");
+  }
 };
 
 const fnI9 = () => {
-    if (letters == "ABCDEFGH") {
-        letters += "I"
-        document.getElementById("letrasId").innerHTML= letters;
-        } else {
-        alert("Accion Invalida")
-    }
-    
+  if (letters == "ABCDEFGH") {
+    letters += "I";
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
+    alert("Accion Invalida");
+  }
 };
 
-
-
 const fnJ0 = () => {
-if (letters == "ABCDEFGHI") {
-    letters += "J"
-    document.getElementById("letrasId").innerHTML= letters;
-    } else {
+  if (letters == "ABCDEFGHI") {
+    letters += "J";
+    document.getElementById("letrasId").innerHTML = letters;
+  } else {
     alert("Accion Invalida");
-   
-} 
+  }
 };
 
 const loadNumbers = () => {
-  numbers = ""
-  if (showNumbers) { for (let i = 1; i <= 10; i++) {
-    numbers += i.toString();
-   document.getElementById("numerosId").innerHTML = numbers;
-    
+  numbers = "";
+  if (letters == "ABCDEFGHIJ") {
+    for (let i = 1; i <= 10; i++) {
+      numbers += i.toString();
+      document.getElementById("numerosId").innerHTML = numbers;
+    }
   }
-    
-  }
-
-
-};
-
-const fnClear = () => {
- if (letters == "ABCDEFGHIJ" && numbers == "12345678910") {
-  letters = ""
-  numbers = ""
-  document.getElementById("letrasId").innerHTML = ""
-  document.getElementById("numerosId").innerHTML = ""
-  
- } else {
-  alert("Accion Invalida")
- }
-
 };
 
 const fnEnter = () => {
   if (letters == "ABCDEFGHIJ") {
-  loadNumbers();
-    
+    loadNumbers();
+  } else {
+    alert("Accion Invalida");
   }
-}; 
+};
 
-
-
+const fnClear = () => {
+  if (letters == "ABCDEFGHIJ" && numbers == "12345678910") {
+    letters = "";
+    numbers = "";
+    document.getElementById("letrasId").innerHTML = "";
+    document.getElementById("numerosId").innerHTML = "";
+  } else {
+    alert("Accion Invalida");
+  }
+};
 
 // eventos
 btnA1.onclick = function () {
@@ -187,17 +176,16 @@ btnH8.onclick = function () {
 };
 btnI9.onclick = function () {
   // funcion que se activa con el boton  I-9
-fnI9();
+  fnI9();
 };
 btnJ0.onclick = function () {
   // funcion que se activa con el boton  J-0
-fnJ0();
+  fnJ0();
 };
 btnClear.onclick = function () {
   // funcion que se activa con el boton  clear
   fnClear();
 };
- btnOk.onclick = function () {
-
+btnOk.onclick = function () {
   fnEnter();
- };
+};
